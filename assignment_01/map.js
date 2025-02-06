eeb0020f237640424771d3f46b8f0aea4dbfbdff
Dingdang K <hk954@cornell.edu>
2025-02-05 19:11:14 -0500

    Add files via upload

---
+++ assignment_01/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'mapbox://styles/dingdangk/cm6jlzcad005a01s703js1n6p';
+const map = new mapboxgl.Map({
+    container: 'map', 
+    style: 'pk.eyJ1IjoiZGluZ2RhbmdrIiwiYSI6ImNrN21ub3pjMDA1c2Qza3BjMmRhdHlrZmEifQ.u1B-kKPnFowpPK_7mCYy_g', 
+    center: [-74.000,43.000],
+    zoom: 10
+});
